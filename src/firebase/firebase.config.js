baf24ec008d36d75bebfbbe509f26f43c48f4f36
Din Islam Rajin <dirrazin572@gmail.com>
2022-11-27 19:14:19 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDogsBfCh5SOPl2946ZSf_3MZpPmavzu04",
+  authDomain: "mobile-mart-424c4.firebaseapp.com",
+  projectId: "mobile-mart-424c4",
+  storageBucket: "mobile-mart-424c4.appspot.com",
+  messagingSenderId: "1070746364541",
+  appId: "1:1070746364541:web:35bfc2d28ba6b418db9f14"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
