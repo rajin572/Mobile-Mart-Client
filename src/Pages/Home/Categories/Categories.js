07bfa03e07ec7684ca9f4b72f37a867f5aed5b62
Din Islam Rajin <dirrazin572@gmail.com>
2022-11-28 02:09:30 +0600

    added category

--- src/Pages/Home/Categories/Categories.js
+++ src/Pages/Home/Categories/Categories.js
@@ -1,5 +1,6 @@
-import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import CategoriesCard from './CategoriesCard';
 
 const Categories = () => {
 
@@ -13,8 +14,11 @@ const Categories = () => {
 });
     return (
     <div className=" my-32">
-        <h2 className=" text-4xl lg:text-5xl font-bold text-center">Categories {categories}</h2>
-        <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10 px-10">  
+        <h2 className=" text-4xl lg:text-5xl font-bold text-center">Categories</h2>
+        <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10 px-10"> 
+        {
+          categories.map(category => <CategoriesCard key={category.category_id} category={category}></CategoriesCard>)
+        } 
         </div>
     </div>
     );
--- src/Pages/Home/Categories/categoriesCard.js
+++ src/Pages/Home/Categories/categoriesCard.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React from "react";
 
-const categoriesCard = () => {
-    return (
-        <div className="card bg-base-100 shadow-2xl">
-        <figure>
-          <img className="rounded-lg" src="https://placeimg.com/400/225/arch" alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions w-full">
-            <button className="btn btn-outline w-full">Book Now</button>
-          </div>
+const categoriesCard = ({category}) => {
+    const {name, image} = category
+  return (
+    <div className="card bg-base-100 shadow-2xl">
+      <figure>
+        <img
+          className="rounded-lg"
+          src={image}
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p>You can see all the product of this category by clicking All Products button</p>
+        <div className="card-actions w-full">
+          <button className="btn btn-outline w-full">All Products</button>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default categoriesCard;
