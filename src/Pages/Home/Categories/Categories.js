388fc5ce6c7bbbd9bcf3896d48852fe70d5d66f6
Din Islam Rajin <dirrazin572@gmail.com>
2022-11-28 01:52:16 +0600

    added react query

--- src/Pages/Home/Categories/Categories.js
+++ src/Pages/Home/Categories/Categories.js
@@ -1,55 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const Categories = () => {
+
+  const {data: categories = []} = useQuery({
+    queryKey: ['categories'],
+    queryFn: async() =>{
+        const res = await fetch('http://localhost:5000/categories');
+        const data = await res.json();
+        return data;
+    }
+});
     return (
     <div className=" my-32">
-        <h2 className=" text-4xl lg:text-5xl font-bold text-center">Categories</h2>
-        <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10 px-10">
-  
-  
-  
-          <div className="card bg-base-100 shadow-xl">
-            <figure>
-              <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Buy Now</button>
-              </div>
-            </div>
-          </div>
-  
-          {/* main item */}
-          <div className="card bg-base-100 shadow-2xl">
-            <figure>
-              <img className="rounded-lg" src="https://placeimg.com/400/225/arch" alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions w-full">
-                <button className="btn btn-outline w-full">Book Now</button>
-              </div>
-            </div>
-          </div>
-          {/* main item */}
-  
-          <div className="card bg-base-100 shadow-xl">
-            <figure>
-              <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Buy Now</button>
-              </div>
-            </div>
-          </div>
-  
-          
+        <h2 className=" text-4xl lg:text-5xl font-bold text-center">Categories {categories}</h2>
+        <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10 px-10">  
         </div>
     </div>
     );
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -2,7 +2,7 @@ import Blog from "../../Pages/Blog/Blog";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import SignUp from "../../Pages/Signup/SignUp";
-import PrivateRoute from "../PrivateRoute/PrivateRoute";
+// import PrivateRoute from "../PrivateRoute/PrivateRoute";
 
 const { createBrowserRouter } = require("react-router-dom");
 const { default: Main } = require("../../Layout/Main");
