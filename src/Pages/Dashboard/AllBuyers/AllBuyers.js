0792a3d6bd161df58f03193ee7b0a16b4a74f41f
Din Islam Rajin <dirrazin572@gmail.com>
2022-11-28 18:19:45 +0600

    show buyers

--- src/Pages/Dashboard/AllBuyers/AllBuyers.js
+++ src/Pages/Dashboard/AllBuyers/AllBuyers.js
@@ -15,8 +15,30 @@ const AllBuyers = () => {
     console.log(users);
     return (
         <div>
-            All buyers
-        </div>
+        <h2 className=" text-4xl text-center">All Buyers</h2>
+        <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Email</th>
+                            <th>Name</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, i) => <tr key={user._id}>
+                                <th>{i+1}</th>
+                                <td>{user.email}</td>
+                                <td>{user.name}</td>
+                                <td><button className='btn btn-outline'>Delete</button></td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+      </div>
     );
 };
 
